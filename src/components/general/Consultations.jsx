187a740f28a3e52544a589d1de764f8ba13563d0
CPanozo23 <cpanozo@outlook.com>
2023-09-02 02:36:52 -0400

    render prueba

--- src/components/general/Consultations.jsx
+++ src/components/general/Consultations.jsx
@@ -4,61 +4,33 @@ import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 import { urlGeneral } from '../../helpers/connect_db';
 const Consultations = () => {
-    const [consultationsData, setConsultationsData] = useState(null);
+    const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(true);
 
-    const url = `https://caro-back.onrender.com/consultations/`;
-
-    useEffect(() => {
-        const fetchConsultationsData = async () => {
-            try {
-                const response = await axios.get(url, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                })
-
-                setConsultationsData(response.data.detail)
-                console.log(response.data.detail)
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        }
-
-        fetchConsultationsData()
-    }, [url])
-
-    const backgroundColors = ['bg-verdeclaro', 'bg-celeste', 'bg-rosado']
+  useEffect(() => {
+    // Realizar la solicitud a la URL
+    axios.get('https://caro-back.onrender.com/consultations/')
+      .then((response) => {
+        // Cuando la solicitud sea exitosa, establecer el mensaje en el estado
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        // Manejar errores aquí si es necesario
+        console.error('Error al obtener datos:', error);
+      })
+      .finally(() => {
+        // Indicar que la solicitud ha terminado
+        setLoading(false);
+      });
+  }, []); 
     return (
-        <>
-            <main className="">
-                <h1 className='mx-5'>Tipos de atención</h1>
-
-                <section className='row justify-content-center align-items-center'>
-                    {consultationsData?.map((consultation, index) => {
-                        const backgroundColor = backgroundColors[index % backgroundColors.length]
-
-                        return (
-                            <article className='col-5 col-lg-3 col-md-4 col-sm-5 card m-1 p-0 ' key={index}>
-                                <div className={`${backgroundColor} card-img-top fs-2 py-3 text-center`}>
-                                    {consultation.name}
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title">Duración: {consultation.duration} minutos</h5>
-                                    <p className="card-text fw-bold">{consultation.price.toLocaleString('es-CL', { style: 'currency', currency: 'CLP' })}</p>
-                                    <NavLink to={`/attention/${consultation._id}`}>
-                                        <button type="button" className='btn btn-primary m-1' id={consultation._id}>
-                                            Buscar hora
-                                        </button>
-                                        </NavLink>
-                                </div>
-                            </article>
-                        )
-                    })}
-                </section>
-            </main>
-            <img src={line} className="" />
-
-        </>
+        <div>
+        {loading ? (
+          <p>Cargando...</p>
+        ) : (
+          <p>Mensaje desde el backend: {message}</p>
+        )}
+      </div>
     )
 }
 
